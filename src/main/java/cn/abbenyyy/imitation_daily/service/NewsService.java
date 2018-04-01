5be73d61d57cf28caa97e597cc86f66c93852d18
abbenyyyPC <abbenyyyyyy@qq.com>
2018-04-01 22:55:16 +0800

    添加移动端接口

--- src/main/java/cn/abbenyyy/imitation_daily/service/NewsService.java
+++ src/main/java/cn/abbenyyy/imitation_daily/service/NewsService.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,7 +22,8 @@ import java.util.List;
 public class NewsService {
     private final NewsRepository newsRepository;
     private final NewsColumnRepository newsColumnRepository;
-//    private Logger logger = LoggerFactory.getLogger(NewsService.class);
+    private final int bannerSize = 5;
+    private Logger logger = LoggerFactory.getLogger(NewsService.class);
 
     @Autowired
     public NewsService(NewsRepository newsRepository, NewsColumnRepository newsColumnRepository) {
@@ -31,9 +33,9 @@ public class NewsService {
 
     public List<BannerBean> fetchBannerAndFormat() {
         List<BannerBean> bannerNewsList = new ArrayList<>();
-        List<News> newsList = newsRepository.findByCssColumn(2, PageRequest.of(0, 5));
+        List<News> newsList = newsRepository.findByCssColumn(2, PageRequest.of(0, bannerSize));
         for (News n : newsList) {
-            BannerBean bannerBean = new BannerBean(n,true);
+            BannerBean bannerBean = new BannerBean(n, true);
             bannerNewsList.add(bannerBean);
         }
         return bannerNewsList;
@@ -43,7 +45,7 @@ public class NewsService {
      * 构造首页数据，普通新闻（css_column相加=4）+1个投票、我说类新闻 + 1个新闻栏目 + 后续的普通新闻
      */
     public HomePageBean structureHomePage() {
-        List<News> newsList = newsRepository.findMore(PageRequest.of(0, 25));
+        List<News> newsList = newsRepository.findMore(PageRequest.of(0, 20 + bannerSize));
         filterBannerNews(newsList);
         //构造正式返回数据集合
         List<News> otherNews = newsRepository.findCategoryIdLessThen2(PageRequest.of(0, 1));
@@ -67,22 +69,30 @@ public class NewsService {
                 }
             }
         }
-        return new HomePageBean(homepageData, newsRepository.count() > 25);
+        return new HomePageBean(homepageData, newsRepository.count() > (20 + bannerSize));
     }
 
     public HomePageBean findMoreHomePageData(long lastKey) {
-        List<News> newsList = newsRepository.findMoreAfter(lastKey, PageRequest.of(0, 25));
+        List<News> newsList = newsRepository.findMoreAfter(lastKey, PageRequest.of(0, 20 + bannerSize));
+        boolean has_more = newsList.size() == (20 + bannerSize);
         filterBannerNews(newsList);
-        List<BannerBean> homepageData = new ArrayList<>();
-        for (int i = 0; i < Math.min(20, newsList.size()); i++) {
-            BannerBean homepageDataItem = new BannerBean(newsList.get(i));
-            if(homepageDataItem.getCssColumn() == 0){
-                homepageDataItem.setCssColumn(2L);
-            }
-            homepageData.add(homepageDataItem);
-        }
-        return new HomePageBean(homepageData, newsList.size() == 25);
+        return new HomePageBean(homepageDataContrusctor(newsList), has_more);
     }
+
+    public HomePageBean findMoreHomePageDataByMobile(long lastKey) {
+        List<News> newsList;
+        if (lastKey == 0) {
+            //首页
+            newsList = new ArrayList<>(newsRepository.findAll(PageRequest.of(0, 20 + bannerSize,
+                    new Sort(Sort.Direction.DESC, "createTime"))).getContent());
+        } else {
+            newsList = newsRepository.findMoreAfter(lastKey, PageRequest.of(0, 20 + bannerSize));
+        }
+        boolean has_more = newsList.size() == (20 + bannerSize);
+        filterBannerNews(newsList);
+        return new HomePageBean(homepageDataContrusctor(newsList), has_more);
+    }
+
 
     /**
      * 过滤掉属于Banner的新闻
@@ -90,7 +100,7 @@ public class NewsService {
      * @param newsList 需要过滤的新闻集合
      */
     private void filterBannerNews(List<News> newsList) {
-        List<News> bannerNewsList = newsRepository.findByCssColumn(2, PageRequest.of(0, 5));
+        List<News> bannerNewsList = newsRepository.findByCssColumn(2, PageRequest.of(0, bannerSize));
         //因为新闻查询出来都是按照创建时间由大到小排序好的，所以只需找出newsList里面距离banner新闻第一条的时间最近的一个点
         //就可以从这个index到结尾查找是否有属于banner新闻的新闻
         if (bannerNewsList.size() > 0) {
@@ -106,6 +116,18 @@ public class NewsService {
                 }
             }
         }
+    }
+
+    private List<BannerBean> homepageDataContrusctor(List<News> newsList) {
+        List<BannerBean> homepageData = new ArrayList<>();
+        for (int i = 0; i < Math.min(20, newsList.size()); i++) {
+            BannerBean homepageDataItem = new BannerBean(newsList.get(i));
+            if (homepageDataItem.getCssColumn() == 0) {
+                homepageDataItem.setCssColumn(2L);
+            }
+            homepageData.add(homepageDataItem);
+        }
+        return homepageData;
     }
 
     private int selectSimilar(List<News> newsList, Date bannerMaxTime) {
